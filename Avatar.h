3b253d7915367d63a7551595dae565ae61de0607
yoshuadupin <djosue@unitec.edu>
2017-03-17 17:31:25 -0600

    Cuasi terminado que las Tortugas Leon te bendigan ana

---
+++ Avatar.h
@@ -0,0 +1,7 @@
+class Avatar
+{
+public:
+	Avatar();
+	~Avatar();
+	
+};
--- main.cpp
+++ main.cpp
@@ -14,13 +14,61 @@ bool run();
 void bash(Bender* , Bender*);
 
 int main(){
+	int opcion;
+	vector<Bender*>benders;
 	srand(time(NULL));
-	Bender* maestro1 = new EarthBender("Aang" ,10 ,50, 1000 , 50 , 5);
-	Bender* maestro2 = new FireBender("Suko" ,10 ,50, 100 , 50 , 7);
+	benders.push_back(new EarthBender("Toph" ,25 ,32, 75 , 40 , 1));
+	benders.push_back(new FireBender("Zuko" ,15 ,45, 100 , 50 , 7));
+	benders.push_back(new WaterBender("Katara" ,20 ,60, 80 , 50 , 10));
+	benders.push_back(new AirBender("Aang" ,15 ,45, 100 , 50 , 7));
 	
-	maestro1->ofensa();
+
+	cout<<"1.Desea entrenar Bender o 2.Simular:"<<endl;
+	cin>>opcion;
+	if(opcion == 1){
+		
+		cout<<"Las 4 naciones desean entrenar sus guerreros:"<<endl;
+		cout<<"1.Tierra 2.Fuego 3. Agua 4. Aire "<<endl;
+		string nombre;	
+		int hp;	
+		int normal;
+		int especial;
+		int fuerza;
+		int suerte;
+
+		cout<<"Ingrese nombre:"<<endl;
+		cin>>nombre;
+		cout<<"Ingrese hp:"<<endl;
+		cin>>hp;
+		cout<<"Ingrese Normal:"<<endl;
+		cin>>normal;	
+		cout<<"Ingrese Especial:"<<endl;
+		cin>>especial;
+		cout<<"Ingrese Fuerza:"<<endl;
+		cin>>fuerza;
+		cout<<"Ingrese Suerte:"<<endl;
+		cin>>suerte;
+		if(opcion == 1){
+			benders.push_back(new EarthBender(nombre , hp , normal , especial , fuerza , suerte));
+		}else if(opcion == 2){
+			benders.push_back(new FireBender(nombre , hp , normal , especial , fuerza , suerte));
+		}else if(opcion == 3){
+			benders.push_back(new WaterBender(nombre , hp , normal , especial , fuerza , suerte));
+		}else if(opcion == 4){
+			benders.push_back(new AirBender(nombre , hp , normal , especial , fuerza , suerte));
+		}
+	}else{
+		int indice1;
+		int indice2;
+		cout<<"Ingrese los benders que quiere:"<<endl;
+		cin>>indice1;
+		cin>>indice2;
+
+		bash(benders[indice1], benders[indice2]);
+	}
 	
-	bash(maestro1, maestro2);	
+	
+
 
 
 	//cout<<maestro1->smashHit()<<endl;
@@ -40,8 +88,9 @@ void bash(Bender* at, Bender* def){
 			at->spiar();
 			cout<<">>1.Normal\n>>2.Especial\n>>3.Recovery[Solo Maestros Aguas]\n>>4.Espiar[Maestros Tierras]\n>>5.RUN NIGGA MASTER"<<endl;
 			cin>>decision;
+			at->ofensa();
 			if(decision == 1){
-				at->ofensa();
+				
 				at->ataqueRegular(def);
 				def->defensa(at);
 			}else if(decision == 2){	
@@ -66,8 +115,9 @@ void bash(Bender* at, Bender* def){
 			def->spiar();
 			cout<<">>1.Normal\n>>2.Especial\n>>3.Recovery[Solo Maestros Aguas]\n>>4.Espiar[Maestros Tierras]"<<endl;
 			cin>>decision;
+			def->ofensa();
 			if(decision == 1){
-				def->ofensa();
+				
 				def->ataqueRegular(at);
 				at->defensa(def);
 			}else if(decision == 2){	
@@ -94,9 +144,12 @@ void bash(Bender* at, Bender* def){
 			turno = true;
 		}
 		if(at->getHp()<=0 || def->getHp()<=0){
+			if(at->getHp()<= 0){
+				cout<<"Murio " <<at->getNombre()<<endl;
+			}else if(def->getHp()<= 0){
+				cout<<"Murio " <<def->getNombre()<<endl;
+			}
 			alguienMurio = false;
-		}else{
-			alguienMurio = true;
 		}
 	}while(alguienMurio && !huyo );
 
